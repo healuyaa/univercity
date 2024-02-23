cd40dec37cceb1717bcbbe8a3381f8d92b6595b8
healuyaa <alexaim.leha@gmail.com>
2024-02-24 00:26:02 +0300

    add ssp_2 lab 1

--- project_cmake/GIIS/lab1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/project/project_cmake/GIIS/lab1
-# Build directory: C:/project/project_cmake/GIIS/lab1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- project_cmake/GIIS/test_imgui/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/project/project_cmake/GIIS/test_imgui
-# Build directory: C:/project/project_cmake/GIIS/test_imgui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
