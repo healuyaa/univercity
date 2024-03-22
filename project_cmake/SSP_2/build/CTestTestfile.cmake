d6505d2e90915b5dda864a5d94ae51307982baae
healuyaa <alexaim.leha@gmail.com>
2024-03-22 20:10:57 +0300

    add govno subota 2 and 3 labs

--- project_cmake/SSP_2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/project/project_cmake/SSP_2
-# Build directory: C:/project/project_cmake/SSP_2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
