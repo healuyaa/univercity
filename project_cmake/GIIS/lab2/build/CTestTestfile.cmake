b68628f7ce831ce0cba2b6cc77fdcb9a23168ffc
healuyaa <alexaim.leha@gmail.com>
2024-03-28 10:47:28 +0300

    adding second lab mrzis new sem

--- project_cmake/GIIS/lab2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/project/project_cmake/GIIS/lab2
-# Build directory: C:/project/project_cmake/GIIS/lab2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
